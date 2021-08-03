285083f8b9678639537dd8f929c97e29f2605aea
LexieN <34977021+SylanGH@users.noreply.github.com>
2021-08-02 20:59:17 -0700

    Launch wiki

--- arcanegraph.js
+++ arcanegraph.js
@@ -1,21 +1,27 @@
 loggingMode = false;
 
-DIN = 0;
-DOUT = 1;
+DOUT = 0;
+DIN = 1;
 DIO = 2;
 LADDEDGE = 0;
 LADDVERTEX = 1;
+LREMOVEEDGE = 2;
+LREMOVEVERTEX = 3;
+ξ = "ξ";
+realityAlias = ξ;
 
-function Edge(vertexA, vertexB, direction) {
+function Edge(vertexA, vertexB, direction, weight=1) {
     this.A = vertexA;
     this.B = vertexB;
     this.direction = direction;
+    this.weight = weight;
 
     if(direction == DIN || direction == DIO)
     {
         vertexB.pointsTo.add(vertexA);
         vertexA.pointedToBy.add(vertexB);
     }
+
     if(direction == DOUT || direction == DIO)
     {
         vertexA.pointsTo.add(vertexB);
@@ -52,7 +58,7 @@ function Vertex(symbol) {
 LogStack = [];
 logInit = 1;
 
-function Log(logType, logString, vertexA, vertexB, direction) {
+function Log(logType, logString, vertexA, vertexB, direction=DIO, weight=1) {
     LogStack.push(this);
     this.log = logString;
     this.logType = logType;
@@ -100,24 +106,33 @@ function Log(logType, logString, vertexA, vertexB, direction) {
             this.hashValue = this.vertexA.symbol.getNumericalValue();
             this.hashValue = this.hashValue * this.vertexB.symbol.getNumericalValue();
             this.hashValue = this.hashValue * (direction + 1);
+            this.hashValue = this.hashValue * (weight + 1);
             break;
         case LADDVERTEX:
             this.hashValue = this.vertexA.symbol.getNumericalValue();
             this.hashValue = (this.hashValue * this.logState);
+            break;
+        case LREMOVEEDGE:
+            this.hashValue = this.vertexA.symbol.getNumericalValue();
+            this.hashValue = (this.hashValue >= this.vertexB.symbol.getNumericalValue()) ? (this.hashValue / (this.vertexB.symbol.getNumericalValue())) : ((this.vertexB.symbol.getNumericalValue()) /  this.hashValue);
+            this.hashValue = this.hashValue * (direction + 1);
+            this.hashValue = this.hashValue * (weight + 1);
+            break;
+        case LREMOVEVERTEX:
+            this.hashValue = this.vertexA.symbol.getNumericalValue();
+            this.hashValue = this.hashValue - this.logState;
     }
     this.hashValue += this.logState;
     console.log("hash value set to " + this.hashValue);
 }
 
 Vertices = {};
-Edges = new Set();
 
-function addEdge(vertexA, vertexB, direction) {
-    let newEdge = new Edge(vertexA, vertexB, direction);
-    Edges.add(newEdge);
+function addEdge(vertexA, vertexB, direction=DOUT, weight=1) {
+    let newEdge = new Edge(vertexA, vertexB, direction, weight);
 
     if(loggingMode)
-        new Log(LADDEDGE, "add edge " + vertexA.symbol.symText + " " + vertexB.symbol.symText + " " + direction, vertexA, vertexB, direction);
+        new Log(LADDEDGE, "add edge " + vertexA.symbol.symText + " " + vertexB.symbol.symText + " " + direction, vertexA, vertexB, direction, weight);
 }
 
 function addVertex(symbol) {
@@ -128,6 +143,49 @@ function addVertex(symbol) {
 
     if(loggingMode)
         new Log(LADDVERTEX, "add vertex " + symbol.symText, newVertex, null, 0);
+}
+
+function removeEdge(vertexA, vertexB, direction) {
+    if(direction == DIN || direction == DIO)
+    {
+        vertexB.pointsTo.delete(vertexA);
+        vertexA.pointedToBy.delete(vertexB);
+    }
+
+    if(direction == DOUT || direction == DIO)
+    {
+        vertexA.pointsTo.delete(vertexB);
+        vertexB.pointedToBy.delete(vertexA);
+    }
+
+    new Log(LREMOVEEDGE, "remove edge " + vertexA.symbol.symText + " " + vertexB.symbol.symText + " " + direction, vertexA, vertexB, direction);
+}
+
+function removeVertex(toRemove, autoResolveEdges = false) {
+    if(toRemove.pointsTo.size != 0 || toRemove.pointedToBy.size != 0)
+    {
+        if(!autoResolveEdges)
+        {
+            console.log("Warning: Vertex is pointed to by existing edges. Please remove these, or pass true as the second argument of this function to auto resolve");
+            return;
+        }else
+        {
+            for(let item of toRemove.pointsTo)
+            {
+                removeEdge(toRemove, item, 1);
+            }
+
+            for(let item of toRemove.pointedToBy)
+            {
+                removeEdge(toRemove, item, 0);
+            }
+        }
+    }
+
+    if(loggingMode)
+        new Log(LREMOVEVERTEX, "remove vertex " + toRemove.symText, newVertex, null, 0);
+
+    delete Vertices[toRemove.symText];
 }
 
 function getSigil() {
